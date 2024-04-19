e9a356c434e42aae933fc624917a1e4f3cf13fba
george <georgenzubechi@gmail.com>
2024-04-19 05:26:54 +0100

    first commit

---
+++ function.js
@@ -0,0 +1,28 @@
+function display() {
+  const overlay = document.getElementById("overlay");
+  const contact = document.getElementById("contactcard");
+  if (overlay.classList.contains("hidden")) {
+    overlay.classList.remove("hidden");
+  } else {
+    overlay.classList.add("hidden");
+  }
+  if (contact.classList.contains("hidden")) {
+    contact.classList.remove("hidden");
+  } else {
+    contact.classList.add("hidden");
+  }
+}
+function closeCon() {
+  const overlay = document.getElementById("overlay");
+  const contact = document.getElementById("contactcard");
+  if (overlay.classList.contains("hidden")) {
+    overlay.classList.remove("hidden");
+  } else {
+    overlay.classList.add("hidden");
+  }
+  if (contact.classList.contains("hidden")) {
+    contact.classList.remove("hidden");
+  } else {
+    contact.classList.add("hidden");
+  }
+}
